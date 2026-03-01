eb35997f5b3592a9450d6adc50d6831f302ce6c2
CDPlutoGame <blutoyt@gmail.com>
2026-03-01 10:29:51 +0100

    Aktualisieren von firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
+// Deine Config (Stelle sicher, dass diese Daten 1:1 von Firebase übernommen sind)
 const firebaseConfig = {
-  apiKey: "AIzaSyD3Z_HfQO4XVsbAnL3XCqf_6bkX3Cc21oc",
-  authDomain: "realm-of-beasts.firebaseapp.com",
-  databaseURL: "https://realm-of-beasts-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "realm-of-beasts",
-  storageBucket: "realm-of-beasts.appspot.com",
-  messagingSenderId: "723138830522",
-  appId: "1:723138830522:web:b3ec8a3d8947c25ec66283",
-  measurementId: "G-Q84J12EZHN"
+  apiKey: "DEIN_API_KEY",
+  authDomain: "dein-projekt.firebaseapp.com",
+  projectId: "dein-projekt-id",
+  storageBucket: "dein-projekt.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getDatabase(app);
+export const db = getFirestore(app);
